90030f649bb5a0f04b5e6a6d3af1da3298d7d330
Octavio Massa <alonsomassa45@gmail.com>
2024-11-08 20:01:45 +0100

    ADDED TICK

--- act.pas
+++ act.pas
@@ -10,10 +10,10 @@ Implementation
 function handleInput(keyPressed:String):TActs;
 begin
     //Suivant la touche appuyée on effectue différente action
-    case keyPressed of
+    case keyPressed of // VERY FRENCH LAYOUT
         'q': 
         begin 
-            handleInput := WALK_LEFT; 
+            handleInput := WALK_LEFT;
         end;
         'd': 
         begin 
@@ -26,6 +26,35 @@ begin
         's': 
         begin 
             handleInput := CROUCH; 
+        end;
+    end;
+end;
+
+procedure act(velocity: TVelocity, blockBelow: Boolean, acts: actsArray);
+var i: Integer;
+    action: TActs;
+begin
+    for i:= 0 to length(acts) - 1 do
+    begin
+        action := acts[i];
+        case action of
+            WALK_LEFT: 
+            begin
+                velocity.x := velocity.x - 1;
+            end;
+            WALK_RIGHT: 
+            begin
+                velocity.x := velocity.x + 1;
+            end;
+            JUMP: 
+            begin
+                if blockBelow then
+                    velocity.y := velocity.y + 10;
+            end;
+            CROUCH: 
+            begin
+                //On ne fait rien
+            end;
         end;
     end;
 end;
---
+++ tick.pas
@@ -0,0 +1,84 @@
+unit Tick;
+
+interface
+
+uses LMMTypes, fileHandler, act;
+
+uses
+    SysUtils;
+
+procedure tick(world: TWorld, acts: TActs);
+
+implementation
+
+procedure tick(world: TWorld, acts: TActs);
+var playerPos: TPosition;
+    playerVel: TVelocity;
+    playerHealth, time: Integer;
+    blockLeft, blockRight, blockBelow: Boolean;
+begin
+    playerPos := world.player.pos;
+    playerVel := world.player.vel;
+    playerHealth := world.player.health;
+    time := world.time;
+
+    blockLeft := world.chunks[1].layout[Round(playerPos.x) - 1][Trunc(playerPos.y)] > 0; // BUG: playerPos.x or playerPos.y will round wierldly and stop the player from moving in any direction TRUNCATE MIGHT ALSO BE WRONG BUT I'M NOT SURE
+    blockRight := world.chunks[1].layout[Round(playerPos.x) + 1][Trunc(playerPos.y)] > 0;
+    blockBelow := world.chunks[1].layout[Round(playerPos.x)][Trunc(playerPos.y)] > 0;
+
+    // Enacting layer input
+    act(playerVel, blockBelow, acts);
+
+    // Collision detection
+    if blockBelow then
+        if playerVel.y < 0 then
+            playerVel.y := 0;
+    if blockLeft then
+        if playerVel.x < 0 then
+            playerVel.x := 0;
+    if blockRight then
+        if playerVel.x > 0 then
+            playerVel.x := 0;
+
+    // Max running speed
+    if playerVel.x > 5 then
+        playerVel.x := 5;
+    if playerVel.x < -5 then
+        playerVel.x := -5;
+    
+    // Terminal Velocity
+    if playerVel.y > 100 then
+        playerVel.y := 100;
+    if playerVel.y < -100 then
+        playerVel.y := -100;
+    
+    // Updating player position
+    playerPos.x := playerPos.x + playerVel.x;
+    playerPos.y := playerPos.y + playerVel.y;
+
+    // Friction
+    if playerVel.x > 0 then
+        playerVel.x := playerVel.x - 0.1;
+    if playerVel.x < 0 then
+        playerVel.x := playerVel.x + 0.1;
+
+    // Gravity
+    playerVel.y := playerVel.y - 9.81/60;
+    
+    
+    playerHealth := playerHealth + 1;
+
+
+    world.player.pos := playerPos;
+    world.player.vel := playerVel;
+    world.player.health := playerHealth;
+
+
+    if time = 24000 then
+        time := 0
+        worldSave(world);
+    else
+        time := time + 1;
+end;
+
+end.
